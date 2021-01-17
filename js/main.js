6154750868b3927294a313eb0468a27c1961ff3e
roman <romantcvetkov@gmail.com>
2021-01-17 23:32:02 +0200

    add arrow

--- js/main.js
+++ js/main.js
@@ -51,6 +51,26 @@ $(function(){
         ]
     });
 
+    $(window).scroll(function() {
+
+        if($(this).scrollTop() != 0) {
+
+            $('#toTop').fadeIn();
+
+        } else {
+
+            $('#toTop').fadeOut();
+
+        }
+
+    });
+
+    $('#toTop').click(function() {
+
+        $('body,html').animate({scrollTop:0},0);
+
+    });
+
 });
 
 let menuParents = document.querySelectorAll('.menu__parent');
